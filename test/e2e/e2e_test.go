03270ec5b765049215007fed0ee15f8929065df2
Peter Kurfer <peter@icb4dc0.de>
2025-01-20 17:20:04 +0100

    chore: add license header to all files

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2024 Peter Kurfer.
+Copyright 2025 Peter Kurfer.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -93,27 +93,27 @@ var _ = Describe("Manager", Ordered, func() {
 			cmd := exec.Command("kubectl", "logs", controllerPodName, "-n", namespace)
 			controllerLogs, err := utils.Run(cmd)
 			if err == nil {
-				_, _ = fmt.Fprintf(GinkgoWriter, fmt.Sprintf("Controller logs:\n %s", controllerLogs))
+				_, _ = fmt.Fprint(GinkgoWriter, "%s", fmt.Sprintf("Controller logs:\n %s", controllerLogs))
 			} else {
-				_, _ = fmt.Fprintf(GinkgoWriter, fmt.Sprintf("Failed to get Controller logs: %s", err))
+				_, _ = fmt.Fprint(GinkgoWriter, "%s", fmt.Sprintf("Failed to get Controller logs: %s", err))
 			}
 
 			By("Fetching Kubernetes events")
 			cmd = exec.Command("kubectl", "get", "events", "-n", namespace, "--sort-by=.lastTimestamp")
 			eventsOutput, err := utils.Run(cmd)
 			if err == nil {
-				_, _ = fmt.Fprintf(GinkgoWriter, fmt.Sprintf("Kubernetes events:\n%s", eventsOutput))
+				_, _ = fmt.Fprint(GinkgoWriter, "%s", fmt.Sprintf("Kubernetes events:\n%s", eventsOutput))
 			} else {
-				_, _ = fmt.Fprintf(GinkgoWriter, fmt.Sprintf("Failed to get Kubernetes events: %s", err))
+				_, _ = fmt.Fprint(GinkgoWriter, "%s", fmt.Sprintf("Failed to get Kubernetes events: %s", err))
 			}
 
 			By("Fetching curl-metrics logs")
 			cmd = exec.Command("kubectl", "logs", "curl-metrics", "-n", namespace)
 			metricsOutput, err := utils.Run(cmd)
 			if err == nil {
-				_, _ = fmt.Fprintf(GinkgoWriter, fmt.Sprintf("Metrics logs:\n %s", metricsOutput))
+				_, _ = fmt.Fprint(GinkgoWriter, "%s", fmt.Sprintf("Metrics logs:\n %s", metricsOutput))
 			} else {
-				_, _ = fmt.Fprintf(GinkgoWriter, fmt.Sprintf("Failed to get curl-metrics logs: %s", err))
+				_, _ = fmt.Fprint(GinkgoWriter, "%s", fmt.Sprintf("Failed to get curl-metrics logs: %s", err))
 			}
 
 			By("Fetching controller manager pod description")
